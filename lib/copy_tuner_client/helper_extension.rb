47b969b44a854c063d7b811ccceabe8e867406e7
aki <lala.akira@gmail.com>
2023-04-06 21:09:30 +0900

    refactor: Add TODO comment

--- lib/copy_tuner_client/helper_extension.rb
+++ lib/copy_tuner_client/helper_extension.rb
@@ -6,6 +6,7 @@ module CopyTunerClient
           def translate_with_copyray_comment(key, **options)
             source = translate_without_copyray_comment(key, **options)
 
+            # TODO: test
             # NOTE: default引数が設定されている場合は、copytunerキャッシュの値をI18n.t呼び出しにより上書きしている
             # SEE: https://github.com/rails/rails/blob/6c43ebc220428ce9fc9569c2e5df90a38a4fc4e4/actionview/lib/action_view/helpers/translation_helper.rb#L82
             I18n.t(key, **options) if options.key?(:default)
