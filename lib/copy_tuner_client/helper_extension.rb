bbf27c98df195f8021c9cf69eeb02b10b283e375
aki <lala.akira@gmail.com>
2023-05-04 06:32:50 +0900

    fix: 相対パス指定の場合の考慮漏れ

--- lib/copy_tuner_client/helper_extension.rb
+++ lib/copy_tuner_client/helper_extension.rb
@@ -13,7 +13,9 @@ module CopyTunerClient
             # TODO: test
             # NOTE: default引数が設定されている場合は、copytunerキャッシュの値をI18n.t呼び出しにより上書きしている
             # SEE: https://github.com/rails/rails/blob/6c43ebc220428ce9fc9569c2e5df90a38a4fc4e4/actionview/lib/action_view/helpers/translation_helper.rb#L82
-            I18n.t(key, **options) if options.key?(:default)
+            if options.key?(:default)
+              I18n.t(key.to_s.first == '.' ? scope_key_by_partial(key) : key, **options)
+            end
 
             if CopyTunerClient.configuration.disable_copyray_comment_injection
               source
