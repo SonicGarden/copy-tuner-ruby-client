aa6f9826667c99b077b06d74085a161ff4557f6a
aki <lala.akira@gmail.com>
2026-06-03 18:06:06 +0900

    bump [skip ci]

--- lib/copy_tuner_client/version.rb
+++ lib/copy_tuner_client/version.rb
@@ -1,6 +1,6 @@
 module CopyTunerClient
   # Client version
-  VERSION = '1.1.5'.freeze
+  VERSION = '1.2.0'.freeze
 
   # API version being used to communicate with the server
   API_VERSION = '2.0'.freeze
