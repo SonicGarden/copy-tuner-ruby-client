7d1e412d10fae15b7a45a60a960603cd4bef9995
aki <lala.akira@gmail.com>
2026-07-01 21:16:51 +0900

    fix: install_hook のメソッド定義順序を修正し NameError を防ぐ
    rubocop の safe autocorrect（Style/ClassMethodsDefinitions）により
    translate_with_copy_tuner_hook の定義が class << self ブロックの外・
    alias_method 群より後ろに移動し、enable_middleware? が true のとき
    alias_method が未定義メソッドを参照して NameError が発生する状態に
    なっていた。既存の rspec は install_hook を呼び出していなかったため
    テストは green のまま回帰を検出できていなかった。
    
    alias_method は対象メソッドが定義済みであることを要求するため、
    translate_with_copy_tuner_hook の定義を alias_method 群より前
    （class << self ブロック内）に戻した。この構造上どうしても
    Style/ClassMethodsDefinitions に抵触するため、該当箇所のみ
    rubocop:disable/enable でピンポイントに無効化した。
    
    あわせて install_hook が enable_middleware? 時に例外を起こさず、
    I18n.translate をフックして TranslationLog に記録することを
    検証するテストを追加した。

--- lib/copy_tuner_client/translation_log.rb
+++ lib/copy_tuner_client/translation_log.rb
@@ -23,26 +23,29 @@ module CopyTunerClient
 
     def self.install_hook
       I18n.class_eval do
+        # alias_method が参照するため、alias_method 群より先に定義する必要がある
+        # rubocop:disable Style/ClassMethodsDefinitions
         class << self
+          def translate_with_copy_tuner_hook(key = nil, **options)
+            scope = options[:scope]
+            scope = scope.dup if scope.is_a?(Array) || scope.is_a?(String)
+            result = translate_without_copy_tuner_hook(key, **options)
+
+            if key.is_a?(Array)
+              key.zip(result).each { |k, v| CopyTunerClient::TranslationLog.add(I18n.normalize_keys(nil, k, scope).compact.join('.'), v) unless v.is_a?(Array) }
+            else
+              CopyTunerClient::TranslationLog.add(I18n.normalize_keys(nil, key, scope).compact.join('.'), result) unless result.is_a?(Array)
+            end
+            result
+          end
+
           if CopyTunerClient.configuration.enable_middleware?
             alias_method :translate_without_copy_tuner_hook, :translate
             alias_method :translate, :translate_with_copy_tuner_hook
             alias_method :t, :translate
           end
         end
-
-        def self.translate_with_copy_tuner_hook(key = nil, **options)
-          scope = options[:scope]
-          scope = scope.dup if scope.is_a?(Array) || scope.is_a?(String)
-          result = translate_without_copy_tuner_hook(key, **options)
-
-          if key.is_a?(Array)
-            key.zip(result).each { |k, v| CopyTunerClient::TranslationLog.add(I18n.normalize_keys(nil, k, scope).compact.join('.'), v) unless v.is_a?(Array) }
-          else
-            CopyTunerClient::TranslationLog.add(I18n.normalize_keys(nil, key, scope).compact.join('.'), result) unless result.is_a?(Array)
-          end
-          result
-        end
+        # rubocop:enable Style/ClassMethodsDefinitions
       end
     end
   end
