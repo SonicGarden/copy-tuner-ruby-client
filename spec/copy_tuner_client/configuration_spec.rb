6949bbfcbdd6db5cf6ed9784b2522604f5f3fcbe
aki <lala.akira@gmail.com>
2026-07-06 20:42:55 +0900

    fix: RuboCop RSpec/RemoveConst の違反を解消する
    remove_const を手動で呼ぶ独自の定数定義/削除機構を、RSpec 標準の
    stub_const に置き換えた。stub_const は定数の有無に応じた
    定義・復元を自前で処理するため、DefinesConstants モジュールの
    before/after フックや @defined_constants によるトラッキング、
    Rails 定数の有無判定（rails_defined）は不要になった。

--- spec/copy_tuner_client/configuration_spec.rb
+++ spec/copy_tuner_client/configuration_spec.rb
@@ -421,19 +421,13 @@ describe CopyTunerClient::Configuration, 'applied with locale filter' do
 end
 
 describe CopyTunerClient::Configuration, 'applied with Rails i18n config' do
-  let!(:rails_defined) { Object.const_defined?(:Rails) }
-
   def self.with_config(i18n_options)
     before do
-      Object.const_set :Rails, Module.new unless rails_defined
+      stub_const('Rails', Module.new)
       i18n = double('i18n', i18n_options)
       config = double('config', i18n:)
       application = double('application', config:)
       allow(Rails).to receive(:application).and_return(application)
-    end
-
-    after do
-      Object.send(:remove_const, :Rails) unless rails_defined
     end
   end
 
--- spec/support/defines_constants.rb
+++ spec/support/defines_constants.rb
@@ -1,12 +1,4 @@
 module DefinesConstants
-  def self.included(example_group)
-    super
-    example_group.class_eval do
-      before { @defined_constants = [] }
-      after { undefine_constants }
-    end
-  end
-
   def define_class(class_name, base = Object, &)
     class_name = class_name.to_s.camelize
     klass = Class.new(base)
@@ -16,29 +8,6 @@ module DefinesConstants
   end
 
   def define_constant(path, value)
-    parse_constant(path) do |parent, name|
-      parent.const_set(name, value)
-    end
-
-    @defined_constants << path
-    value
-  end
-
-  def parse_constant(path)
-    parent_names = path.split('::')
-    name = parent_names.pop
-    parent =
-      parent_names.inject(Object) do |ref, child_name|
-        ref.const_get(child_name)
-      end
-    yield(parent, name)
-  end
-
-  def undefine_constants
-    @defined_constants.reverse.each do |path|
-      parse_constant(path) do |parent, name|
-        parent.send(:remove_const, name)
-      end
-    end
+    stub_const(path, value)
   end
 end
